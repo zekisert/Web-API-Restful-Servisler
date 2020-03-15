9a164d62cf0fd05918b1c3a30ba462d8ff3a1856
zekisert <49846775+zekisert@users.noreply.github.com>
2020-03-16 00:42:58 +0300

    ProductsController.cs Api Controller Class
    Api Controller Class Yapılan İşlemler

---
+++ ProductsController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiDemo.DataAccess;
+using WebApiDemo.Entities;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController : Controller
+    {
+        IProductDal _productDal;
+        public ProductsController(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var products = _productDal.GetList();
+            return Ok(products);
+        }
+
+        [HttpGet("{productId}")]
+        public IActionResult Get(int productId)
+        {
+            try
+            {
+                var product = _productDal.Get(p => p.ProductId == productId);
+
+                if (product == null)
+                {
+                    return NotFound($"There is no product Id = {productId}");
+                }
+                return Ok(product);
+            }
+            catch { }
+
+            return BadRequest();
+            
+            
+        }
+
+        public IActionResult Post(Product product)
+        {
+            try
+            {
+                _productDal.Add(product);
+                return new StatusCodeResult(201);
+            }
+            catch 
+            {
+
+                
+            }
+            return BadRequest();
+        }
+    }
+}
